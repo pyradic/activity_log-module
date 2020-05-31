34d0c5d9eeaed88672745255ddba814e6d6a1c6a
Robin Radic <rradic@hotmail.com>
2020-05-31 08:24:03 +0200

    CRVS-2 #time 4h

---
+++ src/Activity/Event/ActivityWasSaving.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Pyro\ActivityLogModule\Activity\Event;
+
+use \Pyro\ActivityLogModule\Activity\Contract\ActivityInterface;
+
+/**
+ *
+ */
+class ActivityWasSaving
+{
+
+    /**
+     * @var \Pyro\ActivityLogModule\Activity\Contract\ActivityInterface
+     */
+    protected $activity;
+
+
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+    public function __construct(ActivityInterface $activity)
+    {
+        $this->activity = $activity;
+    }
+
+}
